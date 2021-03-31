d97c56f22bd84114a57ee9099dd3b19eb0d96811
Mircea Bereveanu <mircea.bereveanu@gmail.com>
2021-03-31 23:36:07 +0300

    removed bad code

--- client/src/custom/components/pages/home/mailBox-content/mailBox-content.component.ts
+++ client/src/custom/components/pages/home/mailBox-content/mailBox-content.component.ts
@@ -22,30 +22,6 @@ export class MailBoxContentComponent implements OnInit {
 
   ngOnInit(): void {}
 
-  async test() {
-    const ipAPI = '//api.ipify.org?format=json';
-
-    const inputValue = await fetch(ipAPI)
-      .then((response) => response.json())
-      .then((data) => data.ip);
-
-    const { value: ipAddress } = await swal.fire({
-      title: 'Enter your IP address',
-      input: 'text',
-      inputLabel: 'Your IP address',
-      inputValue: inputValue,
-      showCancelButton: true,
-      inputValidator: (value) => {
-        if (!value) {
-          return 'You need to write something!';
-        }
-      },
-    });
-
-    if (ipAddress) {
-      swal.fire(`Your IP address is ${ipAddress}`);
-    }
-  }
   addMailBox(): void {
     this.serviceAuth.reqMailBox(this.form.value);
   }
